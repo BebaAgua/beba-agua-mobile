1b5d64a2d044d8fa90a91a8186a073a5ac244d35
Rodrigo Oliveira <rodrigo@Rodrigo-MacBook-Pro.local>
2023-03-23 22:52:34 -0300

    Added React Navigation library

--- src/screens/Welcome /index.tsx
+++ src/screens/Welcome /index.tsx
@@ -1,9 +1,9 @@
 import { Text, View } from "react-native";
 
-export default function App() {
+export function Welcome() {
   return (
     <View>
-      <Text>Hello World</Text>
+      <Text>Welcome</Text>
     </View>
   );
 }
